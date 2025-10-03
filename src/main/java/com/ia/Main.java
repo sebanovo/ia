35bbc99ed8bfb61d11c4d621abefe5db31d60273
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-10-03 11:58:14 -0400

    delete main

--- src/main/java/com/ia/Main.java
+++ src/main/java/com/ia/Main.java
@@ -28,62 +28,14 @@ public class Main {
     // Rey
     Laberinto.laberintoReyA(m, 0, 0, a - 1, b - 1, 1);
     System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoReyB(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoReyC(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoReyD(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    LinkedList<int[][]> L = new LinkedList<>();
-    Laberinto.laberintoRey(m, L, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + L.size());
-    // Caballo
-    Laberinto.laberintoCaballoA(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoCaballoB(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoCaballoC(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoCaballoD(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    LinkedList<int[][]> L = new LinkedList<>();
-    Laberinto.laberintoCaballo(m, L, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + L.size());
-    // Torres
-    Laberinto.laberintoTorresA(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoTorresB(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoTorresC(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoTorresD(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    LinkedList<int[][]> L = new LinkedList<>();
-    Laberinto.laberintoTorres(m, L, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + L.size());
-    // Alfil
-    Laberinto.laberintoAlfilA(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoAlfilB(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoAlfilC(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoAlfilD(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    LinkedList<int[][]> L = new LinkedList<>();
-    Laberinto.laberintoAlfil(m, L, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + L.size());
-    // Dama
-    Laberinto.laberintoDamaA(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoDamaB(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoDamaC(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    Laberinto.laberintoDamaD(m, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + Laberinto.c);
-    LinkedList<int[][]> L = new LinkedList<>();
-    Laberinto.laberintoDama(m, L, 0, 0, a - 1, b - 1, 1);
-    System.out.println("Cantidad: " + L.size());
+    // Laberinto.laberintoReyB(m, 0, 0, a - 1, b - 1, 1);
+    // System.out.println("Cantidad: " + Laberinto.c);
+    // Laberinto.laberintoReyC(m, 0, 0, a - 1, b - 1, 1);
+    // System.out.println("Cantidad: " + Laberinto.c);
+    // Laberinto.laberintoReyD(m, 0, 0, a - 1, b - 1, 1);
+    // System.out.println("Cantidad: " + Laberinto.c);
+    // LinkedList<int[][]> L = new LinkedList<>();
+    // Laberinto.laberintoRey(m, L, 0, 0, a - 1, b - 1, 1);
+    // System.out.println("Cantidad: " + L.size());
   }
 }
