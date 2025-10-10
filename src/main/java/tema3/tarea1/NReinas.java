809fca217491e0bed52d341c8ee8bb2832ff2781
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-10-09 23:42:54 -0400

    fixed method

--- src/main/java/tema3/tarea1/NReinas.java
+++ src/main/java/tema3/tarea1/NReinas.java
@@ -59,98 +59,29 @@ public class NReinas {
     return true;
   }
 
-  public static boolean hayJaqueMateDama(int[][] m, int i, int j) {
+  public static boolean hayDamaEnRango(int[][] m, int i, int j) {
     return !estanDiagnoalesVacias(m, i, j) || !estanRectasVacias(m, i, j);
   }
 
   public static boolean posValida(int[][] m, int i, int j) {
-    return i >= 0 && i < m.length && j >= 0 && j < m[i].length && m[i][j] == 0 && !hayJaqueMateDama(m, i, j);
+    return i >= 0 && i < m.length && j >= 0 && j < m[i].length && m[i][j] == 0 && !hayDamaEnRango(m, i, j);
   }
 
-  public static Regla elegirRegla(LinkedList<Regla> L1, int[][] m) {
+  public static Regla elegirReglaA(LinkedList<Regla> L1, int[][] m) {
     return L1.removeFirst();
   }
 
-  public static double distancia(int x1, int y1, int x2, int y2) {
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  }
-
-  public static Regla elegirMejorRegla(LinkedList<Regla> L1, int[][] m) {
+  public static Regla elegirReglaB(LinkedList<Regla> L1, int[][] m) {
     return L1.remove(L1.size() / 2);
   }
 
-  public static LinkedList<Regla> reglasAplicablesTorres(int[][] m, int i, int j) {
-    LinkedList<Regla> L1 = new LinkedList<>();
-    int j1 = j - 1;
-    while (posValida(m, i, j1)) { // ⬆
-      L1.add(new Regla(i, j1));
-      j1 = j1 - 1;
-    }
-    int i1 = i - 1;
-    while (posValida(m, i1, j)) { // ⬅
-      L1.add(new Regla(i1, j));
-      i1 = i1 - 1;
-    }
-
-    j1 = j + 1;
-    while (posValida(m, i, j1)) { // ⬇
-      L1.add(new Regla(i, j1));
-      j1 = j1 + 1;
-    }
-    i1 = i + 1;
-    while (posValida(m, i1, j)) { // ➡
-      L1.add(new Regla(i1, j));
-      i1 = i1 + 1;
-    }
-    return L1;
-  }
-
-  public static LinkedList<Regla> reglasAplicablesAlfil(int[][] m, int i, int j) {
-    LinkedList<Regla> L1 = new LinkedList<>();
-    int i1 = i - 1, j1 = j - 1;
-    while (posValida(m, i1, j1)) { // ↖
-      L1.add(new Regla(i1, j1));
-      i1--;
-      j1--;
-    }
-
-    i1 = i - 1;
-    j1 = j + 1;
-    while (posValida(m, i1, j1)) { // ↙
-      L1.add(new Regla(i1, j1));
-      i1--;
-      j1++;
-    }
-
-    i1 = i + 1;
-    j1 = j - 1;
-    while (posValida(m, i1, j1)) { // ↗
-      L1.add(new Regla(i1, j1));
-      i1++;
-      j1--;
-    }
-
-    i1 = i + 1;
-    j1 = j + 1;
-    while (posValida(m, i1, j1)) { // ↘
-      L1.add(new Regla(i1, j1));
-      i1++;
-      j1++;
-    }
-
-    return L1;
-  }
-
   public static LinkedList<Regla> reglasAplicablesDama(int[][] m, int i, int j) {
-    LinkedList<Regla> L = new LinkedList<>();
-
-    if (posValida(m, i, j)) {
-      L.add(new Regla(i, j));
+    LinkedList<Regla> L1 = new LinkedList<>();
+    for (int k = 0; k < m[i].length; k++) {
+      if (posValida(m, i, k))
+        L1.add(new Regla(i, k));
     }
-    L.addAll(reglasAplicablesTorres(m, i, j));
-    L.addAll(reglasAplicablesAlfil(m, i, j));
-
-    return L;
+    return L1;
   }
 
   public static int vueltas = 0;
@@ -161,9 +92,9 @@ public class NReinas {
 
     LinkedList<Regla> L1 = reglasAplicablesDama(m, paso - 1, paso - 1);
     while (!L1.isEmpty()) {
-      Regla R = elegirRegla(L1, m);
+      Regla R = elegirReglaA(L1, m);
       m[R.fil][R.col] = paso;
-      if (nReinasConHeuristica(m, paso + 1)) {
+      if (nReinasSinHeuristica(m, paso + 1)) {
         vueltas++;
         return true;
       }
@@ -179,7 +110,7 @@ public class NReinas {
 
     LinkedList<Regla> L1 = reglasAplicablesDama(m, paso - 1, paso - 1);
     while (!L1.isEmpty()) {
-      Regla R = elegirMejorRegla(L1, m);
+      Regla R = elegirReglaB(L1, m);
       m[R.fil][R.col] = paso;
       if (nReinasConHeuristica(m, paso + 1)) {
         return true;
